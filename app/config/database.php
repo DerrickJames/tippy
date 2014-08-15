b8d04bec35da163476aa23be1620c9c5537fdbcb
DerrickJames <dmushangi@gmail.com>
2014-08-16 00:10:50 +0300

    modified app/config/database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'database',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
