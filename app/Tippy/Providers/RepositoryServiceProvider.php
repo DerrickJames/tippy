67370170fc828bfc9768ab244c6a3a827f9c04cd
DerrickJames <dmushangi@gmail.com>
2014-08-18 20:58:34 +0300

    Created the RepositoryServiceProvider

---
+++ app/Tippy/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace Tippy\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register the service pprovider
+	 *
+	 * @return void
+	 **/
+	public function register()
+	{
+		$this->app->bind(
+			'Tippy\Repositories\CategoryRepositoryInterface',
+			'Tippy\Repositories\Eloquent\CategoryRepository'
+		);
+	}
+}
